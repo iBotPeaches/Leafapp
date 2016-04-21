59d8564c8dd459da3e52fb5ac6277ce969f8fa7e
Connor Tumbleson <connor@sourcetoad.com>
2016-04-21 11:53:08 -0400

    forgot zoom view

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     @if (isset($description))
         <meta name="description" content="{{ $description }}">
     @endif
