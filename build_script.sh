82c5da7555fb3b42cea822dac20e657e74b597c4
Connor Tumbleson <connor.tumbleson@gmail.com>
2016-04-22 07:25:32 -0400

    set .env file in front of script

--- build_script.sh
+++ build_script.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
+cp .env.example .env
 npm-cache install;
 #composer install;
 #npm install --cache-min 9999999;
 npm install gulp;
 mysql --user=connor_testing --password=$DB_PASSWORD -BNe "show tables" connor_testing | tr '\n' ',' | sed -e 's/,$//' | awk '{print "SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS " $1 ";SET FOREIGN_KEY_CHECKS = 1;"}' | mysql --user=connor_testing --password=$DB_PASSWORD connor_testing;
 ./artisan key:generate;
-cp .env.example .env
 ./artisan migrate --env=testing;
 ./artisan db:seed --env=testing;
 gulp
