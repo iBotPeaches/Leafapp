0e70467a93d8b8ea83dea6c2fb26acb2d285221d
Connor Tumbleson <connor.tumbleson@gmail.com>
2016-04-22 07:20:31 -0400

    set .env file

--- build_script.sh
+++ build_script.sh
@@ -5,6 +5,7 @@ npm-cache install;
 npm install gulp;
 mysql --user=connor_testing --password=$DB_PASSWORD -BNe "show tables" connor_testing | tr '\n' ',' | sed -e 's/,$//' | awk '{print "SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS " $1 ";SET FOREIGN_KEY_CHECKS = 1;"}' | mysql --user=connor_testing --password=$DB_PASSWORD connor_testing;
 ./artisan key:generate;
+cp .env.example .env
 ./artisan migrate --env=testing;
 ./artisan db:seed --env=testing;
 gulp
