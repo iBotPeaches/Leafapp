7f7007b5f8af2f0abb8f7816a0b56a77a54653d6
Connor Tumbleson <connor.tumbleson@gmail.com>
2018-04-07 19:41:56 -0400

    feat: remove npm in favor of yarn

--- build_script.sh
+++ build_script.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 cp .env.example .env
-npm-cache install;
-#composer install;
-#npm install --cache-min 9999999;
-npm install gulp;
+yarn install
 mysql --user=connor_testing --password=$DB_PASSWORD -BNe "show tables" connor_testing | tr '\n' ',' | sed -e 's/,$//' | awk '{print "SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS " $1 ";SET FOREIGN_KEY_CHECKS = 1;"}' | mysql --user=connor_testing --password=$DB_PASSWORD connor_testing;
 ./artisan key:generate;
 ./artisan migrate --env=testing;
